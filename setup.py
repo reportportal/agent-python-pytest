0bbf1a862304bf6ef0942fbda1294a034d0743c7
Pavel Papou <pavel_papou@epam.com>
2017-05-02 13:33:50 +0300

    setup.py is adjusted
     - "Programming Language :: Python :: 3.3",

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         "Framework :: Pytest",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.0",
-        "Programming Language :: Python :: 3.3"
+        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6"
