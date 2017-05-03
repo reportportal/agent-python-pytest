f12b844d85ca61041cb1ba47f0001a8f89820827
Nikita Voronchev <Nikita Voronchev>
2017-05-03 16:31:10 +0300

    * Now one can log messages from test script at diferent levels (`logger.<loglevel>(...)`). * Fix `'WARN'` code in `loglevel_map`. * Add `logger.CRITICAL` code to `loglevel_map`.

--- pytest_reportportal/service.py
+++ pytest_reportportal/service.py
@@ -27,12 +27,16 @@ class PyTestServiceClass(with_metaclass(Singleton, object)):
         self.RP = None
         self.TEST_ITEM_STACK = []
         self.launch_id = None
+
+        # Map loglevel codes from `logging` module to ReportPortal text names:
         self.loglevel_map = {
-            0: "TRACE",
+            0: "TRACE",  # logging.NOTSET ->  'TRACE'
             10: "DEBUG",
             20: "INFO",
-            30: "WARNING",
-            40: "ERROR"}
+            30: "WARN",
+            40: "ERROR",
+            50: "ERROR",  # logging.CRITICAL ->  'TRACE'
+        }
 
     def init_service(self, endpoint, project, uuid):
 
