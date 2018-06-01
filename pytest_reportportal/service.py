bfc5c3f18a2e21ee35c259ec1019847e9614290e
Pedro Algarvio <pedro@algarvio.me>
2018-06-01 11:28:47 +0100

    Add support for nested hierarchy test items
    * test-filename:
      * Test Suite:
        * Test case
    
    Fixes #47

--- pytest_reportportal/service.py
+++ pytest_reportportal/service.py
@@ -8,6 +8,9 @@ from time import time
 from six import with_metaclass
 from six.moves import queue
 
+from _pytest.main import Session
+from _pytest.python import Class, Function, Instance, Module
+from _pytest.doctest import DoctestItem
 from reportportal_client import ReportPortalServiceAsync
 
 log = logging.getLogger(__name__)
@@ -28,6 +31,7 @@ class Singleton(type):
 
 
 class PyTestServiceClass(with_metaclass(Singleton, object)):
+
     def __init__(self):
         self.RP = None
         try:
@@ -41,6 +45,8 @@ class PyTestServiceClass(with_metaclass(Singleton, object)):
 
         self._errors = queue.Queue()
         self._loglevels = ('TRACE', 'DEBUG', 'INFO', 'WARN', 'ERROR')
+        self._start_stack = []
+        self._finish_stack = []
 
     def init_service(self, endpoint, project, uuid, log_batch_size,
                      ignore_errors, ignored_tags):
@@ -82,9 +88,12 @@ class PyTestServiceClass(with_metaclass(Singleton, object)):
     def terminate_service(self, nowait=False):
         if self.RP is not None:
             self.RP.terminate(nowait)
+            self.RP = None
 
-    def start_launch(
-            self, launch_name, mode=None, tags=None, description=None):
+    def start_launch(self, launch_name,
+                     mode=None,
+                     tags=None,
+                     description=None):
         self._stop_if_necessary()
         if self.RP is None:
             return
@@ -100,13 +109,72 @@ class PyTestServiceClass(with_metaclass(Singleton, object)):
         req_data = self.RP.start_launch(**sl_pt)
         log.debug('ReportPortal - Launch started: response_body=%s', req_data)
 
+    def get_item_parts(self, item):
+        parts = []
+        parent = item.parent
+        if not isinstance(parent, Instance):
+            parts.append(parent)
+        while True:
+            parent = parent.parent
+            if parent is None:
+                break
+            if isinstance(parent, Instance):
+                continue
+            if isinstance(parent, Session):
+                break
+            parts.append(parent)
+
+        parts.reverse()
+        parts.append(item)
+        return parts
+
+
+    def collect_tests(self, session):
+        self._stop_if_necessary()
+        if self.RP is None:
+            return
+
+        for item in session.items:
+            # Start collecting test item parts
+            parts_in = []
+            parts_out = []
+            parts = self.get_item_parts(item)
+            # Add all parts in revers order to parts_out
+            parts_out.extend(reversed(parts))
+            while parts:
+                part = parts.pop(0)
+                if part in self._start_stack:
+                    # If we've seen this part, skip it
+                    continue
+                # We haven't seen this part yet. Could be a Class, Module or Function
+                # Appent to parts_in
+                parts_in.append(part)
+
+            # Update self._start_stack and self._finish_stack
+            self._start_stack.extend(parts_in)
+            self._finish_stack.extend(parts_out)
+
     def start_pytest_item(self, test_item=None):
         self._stop_if_necessary()
         if self.RP is None:
             return
 
+        while True:
+            part = self._start_stack.pop(0)
+            if part is test_item:
+                break
+            payload = {
+                'name': part.name,
+                'description': self._get_description(part),
+                'tags': self._get_tags(part),
+                'start_time': timestamp(),
+                'item_type': 'SUITE'
+            }
+            log.debug('ReportPortal - Start Suite: request_body=%s', payload)
+            self.RP.start_test_item(**payload)
+
         start_rq = {
-            'name': self._get_full_name(test_item),
+            'name': test_item.name,
             'description': self._get_description(test_item),
             'tags': self._get_tags(test_item),
             'start_time': timestamp(),
@@ -132,6 +200,9 @@ class PyTestServiceClass(with_metaclass(Singleton, object)):
         if self.RP is None:
             return
 
+        # Remove the test from the finish stack
+        self._finish_stack.pop(0)
+
         fta_rq = {
             'end_time': timestamp(),
             'status': status,
@@ -140,6 +211,21 @@ class PyTestServiceClass(with_metaclass(Singleton, object)):
 
         log.debug('ReportPortal - Finish TestItem: request_body=%s', fta_rq)
         self.RP.finish_test_item(**fta_rq)
+
+        while self._finish_stack:
+            if isinstance(self._finish_stack[0], Function):
+                break
+            part = self._finish_stack.pop(0)
+            if self._finish_stack.count(part):
+                continue
+            payload = {
+                'end_time': timestamp(),
+                'issue': issue,
+                'status': 'PASSED'
+            }
+            log.debug('ReportPortal - End TestSuite: request_body=%s', payload)
+            self.RP.finish_test_item(**payload)
+
 
     def finish_launch(self, launch=None, status='rp_launch'):
         self._stop_if_necessary()
@@ -186,9 +272,9 @@ class PyTestServiceClass(with_metaclass(Singleton, object)):
 
     @staticmethod
     def _get_description(test_item):
-        try:
-            # for common items
-            return test_item.function.__doc__
-        except AttributeError:
-            # doctest has no `function` attribute
+        if isinstance(test_item, (Class, Function, Module)):
+            doc = test_item.obj.__doc__
+            if doc is not None:
+                return doc.strip()
+        if isinstance(test_item, DoctestItem):
             return test_item.reportinfo()[2]
