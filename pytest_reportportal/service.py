b44a834ad44ba2bcd07e1f3cd9ccce18f3c714ef
bigbZik <bigbzik@gmail.com>
2018-09-05 00:01:08 -0700

    - Adding new pytest ini option 'rp_tests_tags', to add tags for each items in the launch - Removing docstring indentations for item description (see https://www.python.org/dev/peps/pep-0257/)

--- pytest_reportportal/service.py
+++ pytest_reportportal/service.py
@@ -21,6 +21,32 @@ def timestamp():
     return str(int(time() * 1000))
 
 
+def trim_docstring(docstring):
+    if not docstring:
+        return ''
+    # Convert tabs to spaces (following the normal Python rules)
+    # and split into a list of lines:
+    lines = docstring.expandtabs().splitlines()
+    # Determine minimum indentation (first line doesn't count):
+    indent = sys.maxsize
+    for line in lines[1:]:
+        stripped = line.lstrip()
+        if stripped:
+            indent = min(indent, len(line) - len(stripped))
+    # Remove indentation (first line is special):
+    trimmed = [lines[0].strip()]
+    if indent < sys.maxsize:
+        for line in lines[1:]:
+            trimmed.append(line[indent:].rstrip())
+    # Strip off trailing and leading blank lines:
+    while trimmed and not trimmed[-1]:
+        trimmed.pop()
+    while trimmed and not trimmed[0]:
+        trimmed.pop(0)
+    # Return a single string:
+    return '\n'.join(trimmed)
+
+
 class Singleton(type):
     _instances = {}
 
@@ -385,8 +411,13 @@ class PyTestServiceClass(with_metaclass(Singleton, object)):
     def _get_item_tags(self, item):
         # Try to extract names of @pytest.mark.* decorators used for test item
         # and exclude those which present in rp_ignore_tags parameter
-        return [k for k in item.keywords if item.get_marker(k) is not None
+        #return [k for k in item.keywords if item.get_marker(k) is not None
+        #        and k not in self.ignored_tags]
+        tags = [k for k in item.keywords if item.get_marker(k) is not None
                 and k not in self.ignored_tags]
+        tags.extend(item.session.config.getini('rp_tests_tags'))
+
+        return tags
 
     def _get_parameters(self, item):
         return item.callspec.params if hasattr(item, 'callspec') else {}
@@ -408,6 +439,6 @@ class PyTestServiceClass(with_metaclass(Singleton, object)):
         if isinstance(test_item, (Class, Function, Module, Item)):
             doc = test_item.obj.__doc__
             if doc is not None:
-                return doc.strip()
+                return trim_docstring(doc)
         if isinstance(test_item, DoctestItem):
             return test_item.reportinfo()[2]
