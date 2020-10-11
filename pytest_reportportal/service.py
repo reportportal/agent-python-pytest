c56bf9fac6168a98939cb84cbbc91cdf9b52e42f
Nikita Fedoraev <36140967+NikitaFedoraev@users.noreply.github.com>
2020-10-11 14:57:47 -0700

    Set log_batch_size value provided on init to  self.log_batch_size().

--- pytest_reportportal/service.py
+++ pytest_reportportal/service.py
@@ -142,6 +142,7 @@ class PyTestServiceClass(with_metaclass(Singleton, object)):
             if self.rp_supports_parameters:
                 self.ignored_attributes = list(
                     set(ignored_attributes).union({'parametrize'}))
+            self.log_batch_size = log_batch_size
             log.debug('ReportPortal - Init service: endpoint=%s, '
                       'project=%s, uuid=%s', endpoint, project, uuid)
             self.rp = ReportPortalService(
