9c5c459a45a2f8efdc7fa6165e5c58c354421fd1
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-12-10 12:38:32 +0300

    Test refactoring

--- tests/integration/test_suite_hierarchy.py
+++ tests/integration/test_suite_hierarchy.py
@@ -29,6 +29,20 @@ def item_id_gen(**kwargs):
                              random.randint(0, 9999))
 
 
+def verify_start_item_parameters(mock_client, expected_items):
+    assert mock_client.start_test_item.call_count == len(expected_items), \
+        '"start_test_item" method was called incorrect number of times'
+
+    call_args = mock_client.start_test_item.call_args_list
+    for i, call in enumerate(call_args):
+        start_kwargs = call[1]
+        expect(start_kwargs['name'] == expected_items[i]['name'])
+        expect(start_kwargs['item_type'] == expected_items[i]['item_type'])
+        verification = expected_items[i]['parent_item_id']
+        expect(verification(start_kwargs['parent_item_id']))
+    assert_expectations()
+
+
 @mock.patch(REPORT_PORTAL_SERVICE)
 @pytest.mark.parametrize('suite_name', ('examples/test_simple.py',
                                         'examples/inner/test_inner_simple.py'))
@@ -46,21 +60,14 @@ def test_simple_test_in_folder(mock_client_init, suite_name):
     result = utils.run_pytest_tests(tests=[suite_name])
     assert int(result) == 0, 'Exit code should be 0 (no errors)'
 
-    assert mock_client.start_test_item.call_count == 2, \
-        '"start_test_item" method was called incorrect number of times'
+    expected_items = [
+        {'name': suite_name, 'item_type': 'SUITE',
+         'parent_item_id': lambda x: x is None},
+        {'name': test_name, 'item_type': 'STEP',
+         'parent_item_id': lambda x: x.startswith(suite_name)}
+    ]
 
-    call_args = mock_client.start_test_item.call_args_list
-    suite_start_kwargs = call_args[0][1]
-    test_start_kwargs = call_args[1][1]
-
-    expect(suite_start_kwargs['name'] == suite_name)
-    expect(suite_start_kwargs['item_type'] == 'SUITE')
-    expect(suite_start_kwargs['parent_item_id'] is None)
-
-    expect(test_start_kwargs['name'] == test_name)
-    expect(test_start_kwargs['parent_item_id'].startswith(suite_name))
-    expect(test_start_kwargs['item_type'] == 'STEP')
-    assert_expectations()
+    verify_start_item_parameters(mock_client, expected_items)
 
 
 @mock.patch(REPORT_PORTAL_SERVICE)
@@ -79,27 +86,16 @@ def test_simple_test_in_class(mock_client_init):
     result = utils.run_pytest_tests(tests=[root_suite_name])
     assert int(result) == 0, 'Exit code should be 0 (no errors)'
 
-    assert mock_client.start_test_item.call_count == 3, \
-        '"start_test_item" method was called incorrect number of times'
+    expected_items = [
+        {'name': root_suite_name, 'item_type': 'SUITE',
+         'parent_item_id': lambda x: x is None},
+        {'name': child_suite_name, 'item_type': 'SUITE',
+         'parent_item_id': lambda x: x.startswith(root_suite_name)},
+        {'name': test_name, 'item_type': 'STEP',
+         'parent_item_id': lambda x: x.startswith(child_suite_name)}
+    ]
 
-    call_args = mock_client.start_test_item.call_args_list
-    root_suite_start_kwargs = call_args[0][1]
-    child_suite_start_kwargs = call_args[1][1]
-    test_start_kwargs = call_args[2][1]
-
-    expect(root_suite_start_kwargs['name'] == root_suite_name)
-    expect(root_suite_start_kwargs['item_type'] == 'SUITE')
-    expect(root_suite_start_kwargs['parent_item_id'] is None)
-
-    expect(child_suite_start_kwargs['name'] == child_suite_name)
-    expect(child_suite_start_kwargs['parent_item_id']
-           .startswith(root_suite_name))
-    expect(child_suite_start_kwargs['item_type'] == 'SUITE')
-
-    expect(test_start_kwargs['name'] == test_name)
-    expect(test_start_kwargs['parent_item_id'].startswith(child_suite_name))
-    expect(test_start_kwargs['item_type'] == 'STEP')
-    assert_expectations()
+    verify_start_item_parameters(mock_client, expected_items)
 
 
 @mock.patch(REPORT_PORTAL_SERVICE)
@@ -119,33 +115,18 @@ def test_simple_test_in_class_in_class(mock_client_init):
     result = utils.run_pytest_tests(tests=[root_suite_name])
     assert int(result) == 0, 'Exit code should be 0 (no errors)'
 
-    assert mock_client.start_test_item.call_count == 4, \
-        '"start_test_item" method was called incorrect number of times'
+    expected_items = [
+        {'name': root_suite_name, 'item_type': 'SUITE',
+         'parent_item_id': lambda x: x is None},
+        {'name': outer_child_suite_name, 'item_type': 'SUITE',
+         'parent_item_id': lambda x: x.startswith(root_suite_name)},
+        {'name': inner_child_name, 'item_type': 'SUITE',
+         'parent_item_id': lambda x: x.startswith(outer_child_suite_name)},
+        {'name': test_name, 'item_type': 'STEP',
+         'parent_item_id': lambda x: x.startswith(inner_child_name)}
+    ]
 
-    call_args = mock_client.start_test_item.call_args_list
-    root_suite_start_kwargs = call_args[0][1]
-    outer_child_suite_start_kwargs = call_args[1][1]
-    inner_child_suite_start_kwargs = call_args[2][1]
-    test_start_kwargs = call_args[3][1]
-
-    expect(root_suite_start_kwargs['name'] == root_suite_name)
-    expect(root_suite_start_kwargs['item_type'] == 'SUITE')
-    expect(root_suite_start_kwargs['parent_item_id'] is None)
-
-    expect(outer_child_suite_start_kwargs['name'] == outer_child_suite_name)
-    expect(outer_child_suite_start_kwargs['parent_item_id']
-           .startswith(root_suite_name))
-    expect(outer_child_suite_start_kwargs['item_type'] == 'SUITE')
-
-    expect(inner_child_suite_start_kwargs['name'] == inner_child_name)
-    expect(inner_child_suite_start_kwargs['parent_item_id']
-           .startswith(outer_child_suite_name))
-    expect(inner_child_suite_start_kwargs['item_type'] == 'SUITE')
-
-    expect(test_start_kwargs['name'] == test_name)
-    expect(test_start_kwargs['parent_item_id'].startswith(inner_child_name))
-    expect(test_start_kwargs['item_type'] == 'STEP')
-    assert_expectations()
+    verify_start_item_parameters(mock_client, expected_items)
 
 
 @mock.patch(REPORT_PORTAL_SERVICE)
@@ -166,29 +147,15 @@ def test_simple_tests_in_different_inner_folders(mock_client_init):
     result = utils.run_pytest_tests(tests=tests_to_run)
     assert int(result) == 0, 'Exit code should be 0 (no errors)'
 
-    assert mock_client.start_test_item.call_count == 4, \
-        '"start_test_item" method was called incorrect number of times'
+    expected_items = [
+        {'name': first_suite_name, 'item_type': 'SUITE',
+         'parent_item_id': lambda x: x is None},
+        {'name': test_name, 'item_type': 'STEP',
+         'parent_item_id': lambda x: x.startswith(first_suite_name)},
+        {'name': second_suite_name, 'item_type': 'SUITE',
+         'parent_item_id': lambda x: x is None},
+        {'name': test_name, 'item_type': 'STEP',
+         'parent_item_id': lambda x: x.startswith(second_suite_name)}
+    ]
 
-    call_args = mock_client.start_test_item.call_args_list
-    first_suite_kwargs = call_args[0][1]
-    first_test_kwargs = call_args[1][1]
-    second_suite_kwargs = call_args[2][1]
-    second_test_kwargs = call_args[3][1]
-
-    expect(first_suite_kwargs['name'] == first_suite_name)
-    expect(first_suite_kwargs['item_type'] == 'SUITE')
-    expect(first_suite_kwargs['parent_item_id'] is None)
-
-    expect(second_suite_kwargs['name'] == second_suite_name)
-    expect(second_suite_kwargs['item_type'] == 'SUITE')
-    expect(second_suite_kwargs['parent_item_id'] is None)
-
-    expect(first_test_kwargs['name'] == test_name)
-    expect(first_test_kwargs['parent_item_id'].startswith(first_suite_name))
-    expect(first_test_kwargs['item_type'] == 'STEP')
-
-    expect(second_test_kwargs['name'] == test_name)
-    expect(second_test_kwargs['parent_item_id'].startswith(second_suite_name))
-    expect(second_test_kwargs['item_type'] == 'STEP')
-
-    assert_expectations()
+    verify_start_item_parameters(mock_client, expected_items)
