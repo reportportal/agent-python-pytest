a6c0fec3b73a53d418ee0bc39ea9540490b4a7b8
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2022-02-08 11:58:23 +0300

    Test fix

--- tests/integration/test_pytest_parallel.py
+++ tests/integration/test_pytest_parallel.py
@@ -13,8 +13,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License
 
-from six.moves import mock
 from delayed_assert import expect, assert_expectations
+from six.moves import mock
 
 from tests import REPORT_PORTAL_SERVICE
 from tests.helpers import utils
@@ -31,7 +31,7 @@ def test_pytest_parallel_threads(mock_client_init):
     mock_client.start_test_item.side_effect = item_id_gen
 
     result = utils.run_pytest_tests(tests=['examples/hierarchy'],
-                                    args=["--tests-per-worker", "2"])
+                                    args=['--tests-per-worker', '2'])
     assert int(result) == 0, 'Exit code should be 0 (no errors)'
 
     mock_client = mock_client_init.return_value
@@ -41,18 +41,19 @@ def test_pytest_parallel_threads(mock_client_init):
     expect(mock_client.finish_launch.call_count == 1,
            '"finish_launch" method was not called')
     start_item_called = mock_client.start_test_item.call_count
-    expect(start_item_called == 17,
+    expect(start_item_called == 15,
            '"start_test_item" method was called incorrect number of times: ' +
            str(start_item_called))
 
     finish_item_called = mock_client.finish_test_item.call_count
-    expect(finish_item_called == 17,
+    expect(finish_item_called == 15,
            '"finish_test_item" method was called incorrect number of times: ' +
            str(finish_item_called))
     assert_expectations()
 
     finish_args = mock_client.finish_launch.call_args_list
-    expect(finish_args[0][1]['status'] == 'PASSED', 'Launch failed')
+    expect(finish_args[0][1]['status'] in ('PASSED', None), 'Launch failed')
     launch_end_time = finish_args[0][1]['end_time']
-    expect(launch_end_time is not None and launch_end_time > 0,
+    expect(launch_end_time is not None and int(launch_end_time) > 0,
            'Launch end time is empty')
+    assert_expectations()
