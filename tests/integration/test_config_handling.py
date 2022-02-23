b0267692484768d1ed86ff28778b07f3516b4ee9
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2022-02-23 12:37:15 +0300

    'rp_parent_item_id' test add

--- tests/integration/test_config_handling.py
+++ tests/integration/test_config_handling.py
@@ -1,4 +1,4 @@
-"""This module includes integration tests for 'rp_launch_id' parameter."""
+"""This module includes integration tests for configuration parameters."""
 #  Copyright (c) 2022 https://reportportal.io .
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ from tests.helpers import utils
 
 
 @mock.patch(REPORT_PORTAL_SERVICE)
-def test_empty_run(mock_client_init):
+def test_rp_launch_id(mock_client_init):
     """Verify that RP plugin does not start/stop launch if 'rp_launch_id' set.
 
     :param mock_client_init: Pytest fixture
@@ -44,4 +44,34 @@ def test_empty_run(mock_client_init):
     expect(len(start_call_args) == len(finish_call_args))
     expect(len(start_call_args) == 3)
 
+    assert_expectations()
+
+
+@mock.patch(REPORT_PORTAL_SERVICE)
+def test_rp_parent_item_id(mock_client_init):
+    """Verify RP set Parent ID for root items if 'rp_parent_item_id' set.
+
+    :param mock_client_init: Pytest fixture
+    """
+    parent_id = "parent_id"
+    variables = dict()
+    variables['rp_parent_item_id'] = parent_id
+    variables.update(utils.DEFAULT_VARIABLES.items())
+    result = utils.run_pytest_tests(tests=['examples/test_simple.py'],
+                                    variables=variables)
+
+    assert int(result) == 0, 'Exit code should be 0 (no errors)'
+
+    mock_client = mock_client_init.return_value
+    expect(mock_client.start_launch.call_count == 1,
+           '"start_launch" method was not called')
+    expect(mock_client.finish_launch.call_count == 1,
+           '"finish_launch" method was not called')
+
+    start_call_args = mock_client.start_test_item.call_args_list
+    finish_call_args = mock_client.finish_test_item.call_args_list
+
+    expect(len(start_call_args) == len(finish_call_args))
+    expect(len(start_call_args) == 3)
+    expect(start_call_args[0][1]["parent_item_id"] == parent_id)
     assert_expectations()
