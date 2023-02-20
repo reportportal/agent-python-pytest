b3ce105787d6a036bc2684798632bffe04d271f7
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-02-20 11:21:35 +0300

    Add issue update on item finish

--- pytest_reportportal/service.py
+++ pytest_reportportal/service.py
@@ -632,6 +632,7 @@ class PyTestServiceClass(object):
         :param leaf: item context
         """
         leaf['attributes'] = self._process_attributes(leaf['item'])
+        leaf['issue'] = self._process_issue(item)
 
     def _build_start_step_rq(self, leaf):
         payload = {
