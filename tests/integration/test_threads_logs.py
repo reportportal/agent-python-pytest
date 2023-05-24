def9dd8dab43736bb114c0fca75e1d9ed4582b21
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-05-24 18:09:15 +0300

    Test fixes

--- tests/integration/test_threads_logs.py
+++ tests/integration/test_threads_logs.py
@@ -4,21 +4,21 @@ from tests import REPORT_PORTAL_SERVICE
 from tests.helpers import utils
 
 
-@mock.patch('pytest_reportportal.rp_logging.RPClient')
 @mock.patch(REPORT_PORTAL_SERVICE)
-def test_rp_thread_logs_reporting(mock_client_init, mock_thread_client_init):
+def test_rp_thread_logs_reporting(mock_client_init):
     """Verify logs from threads are sent to correct items`.
 
     :param mock_client_init: Pytest fixture
     """
     mock_client = mock_client_init.return_value
-    mock_thread_client = mock_thread_client_init.return_value
+    mock_thread_client = mock_client.clone()
 
     def init_thread_client(*_, **__):
         from reportportal_client._local import set_current
         set_current(mock_thread_client)
         return mock_thread_client
-    mock_thread_client_init.side_effect = init_thread_client
+
+    mock_client.clone.side_effect = init_thread_client
     result = utils.run_tests_with_client(
         mock_client,
         ['examples/threads/'],
