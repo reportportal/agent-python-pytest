caeaa713246da8acfe7d8ab6f1cec7c44e28789b
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-06-09 14:18:40 +0300

    Remove Python 3.6 and add Python 3.11

--- setup.py
+++ setup.py
@@ -34,11 +34,11 @@ setup(
     keywords=['testing', 'reporting', 'reportportal', 'pytest', 'agent'],
     classifiers=[
         'Framework :: Pytest',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10'
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
         ],
     entry_points={
         'pytest11': [
