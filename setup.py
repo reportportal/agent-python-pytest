55ce972e5a00328face2112f2ee6350c3256f288
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2024-09-05 12:02:53 +0300

    Add Python 3.12 to builders

--- setup.py
+++ setup.py
@@ -51,7 +51,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11'
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12'
         ],
     entry_points={
         'pytest11': [
