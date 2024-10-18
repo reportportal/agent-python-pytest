fcd26672b38a0c7ac8a85f0ab4de4bffa18bb030
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2024-10-18 14:48:11 +0300

    Update examples

---
+++ examples/fixtures/test_failure_fixture_teardown/test_fixture_teardown.py
@@ -0,0 +1,32 @@
+#  Copyright 2024 EPAM Systems
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from time import sleep
+
+
+def test_failure_fixture_teardown(fixture_teardown_config):
+    sleep(0.001)
+    assert fixture_teardown_config is not None
