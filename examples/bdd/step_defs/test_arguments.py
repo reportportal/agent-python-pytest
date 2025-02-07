f3caa44d70acf65bf360374f68646da948f619b3
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2025-02-07 17:23:12 +0300

    Add pytest_bdd example

---
+++ examples/bdd/step_defs/test_arguments.py
@@ -0,0 +1,33 @@
+#  Copyright 2025 EPAM Systems
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from pytest_bdd import scenarios, given, when, then, parsers
+
+
+scenarios("../features/arguments.feature")
+
+
+@given(parsers.parse("there are {start:d} cucumbers"), target_fixture="cucumbers")
+def given_cucumbers(start):
+    return {"start": start, "eat": 0}
+
+
+@when(parsers.parse("I eat {eat:d} cucumbers"))
+def eat_cucumbers(cucumbers, eat):
+    cucumbers["eat"] += eat
+
+
+@then(parsers.parse("I should have {left:d} cucumbers"))
+def should_have_left_cucumbers(cucumbers, left):
+    assert cucumbers["start"] - cucumbers["eat"] == left
